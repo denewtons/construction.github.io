771efa6d1807667535f3e3f78d9d8c8734b9fc12
Newton Edwards Isack Otieno <94446952+denewtons@users.noreply.github.com>
2022-05-26 22:56:27 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>land survey site</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+       <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/ddsmoothmenu.js"></script>
+</head>
+<body>
+    <header>
+        <div class="nav_item">
+           <div class="active"><img src="logo.jpeg" alt=""></div>
+        <div class="topnav" id="myTopnav">
+            <a href=""></a>
+            <a href="#home" class="active">Home</a>
+            <a href="#news" class="active">News</a>
+            <a href="#contact" class="active">Contact</a>
+            <a href="#about" class="active">About</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+          </div>
+        </div>
+    </header>
+    <main class="main--flex">
+        <section class="container">
+            <div class="left-div left-text">
+                <h2>geogolden <br> land surveyors</h2>
+            </div>
+               <div class="right-div right-text">
+                <img src="logo.jpeg" alt="">
+            </div>            
+            
+        </section>  
+        <div id="tooplate_main_top"></div>
+    <div id="tooplate_main">
+        	
+        <div class="col_fw">
+            <div class="col_allw270 fp_service_box">
+                <h3>New Standard</h3>
+                <img src="images/tick-48px.png" alt="Image Tick" />
+                <p> Vivamus a velit. Vivamus leo velit, convallis id, ultrices sit amet, validate <a href="http://validator.w3.org/check?uri=referer" rel="nofollow"><strong>XHTML</strong></a> and <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow"><strong>CSS</strong></a>. </p>
+                <a class="more" href="#">Detail</a>
+            </div>
+			
+            <div class="col_allw270 fp_service_box">
+                <h3>High Quality</h3>
+                <img src="images/post-it-48px.png" alt="Post Image" />
+                <p>Donec pharetra orci id tortor cursus eu ultricies velit vehicula. Phasellus eu ante tellus.</p>
+                <a class="more" href="#">Detail</a>
+            </div>
+			
+            <div class="col_allw270 fp_service_box col_last">
+                <h3>Solid Platform</h3>
+                <img src="images/rosette-48px.png" alt="Rosette" />
+                <p>Curabitur sed lectus id erat viverra consectetur nec in sapien. Etiam vitae tortor mi.</p>
+                <a class="more" href="#">Detail</a>
+            </div>
+			
+            <div class="cleaner"></div>
+        </div>
+		
+		<div class="cleaner"></div>       
+	</div> 
+	
+    <div id="tooplate_main_bottom"></div>
+    
+    <div id="tooplate_bottom">
+     	<div class="col_allw270">
+        	<h4>Pages</h4>
+            <ul class="bottom_list">
+            	<li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About Us</a></li>
+                <li><a href="portfolio.html">Portfolio</a></li>
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="contact.html">Contact</a></li>
+			</ul>
+        </div>
+		
+   	  	<div class="col_allw270">
+        	<h4>Blog Posts</h4>
+            <ul class="bottom_list">
+            	<li><a href="#">Donec placerat odio at leo suscipit hendrerit</a></li>
+            	<li><a href="#">Proin vitae magna eget sem mattis imperdie</a></li>
+            	<li><a href="#">Nunc ac nunc ut erat consectetur rutrum</a></li>
+            	<li><a href="#">Phasellus cursus nulla in risus condimentum </a></li>
+				<li><a href="#">Fusce euismod odio sed mauris hendrerit</a></li>
+			</ul>
+        </div>
+		
+    	<div class="col_allw270 col_last">
+        	<h4>About Us</h4>
+            <p><em>Donec vehicula mauris sed arcu fermentum ornare. Aliquam erat volutpat.</em></p>
+            <p>Orange is one of the <a href="http://www.tooplate.com/free-templates">free templates</a> by tooplate. Credits go to <a href="http://www.photovaco.com">Free Photos</a> for photos and <a href="http://www.thewebdesignblog.co.uk">The Web Design Blog</a> for icons.</p>
+        </div>   
+		             
+    	<div class="cleaner"></div>
+    </div>     
+    </main>
+    <footer>
+         <div class="container">
+             <div class="right-div right-text">
+                  <h4 class="card-title">You can find us on:</h4>
+                  <div class="template-demo">
+                    <button type="button" class="btn btn-social-icon btn-facebook btn-rounded"><i class="fa fa-facebook"></i></button>
+                    <button type="button" class="btn btn-social-icon btn-youtube btn-rounded"><i class="fa fa-youtube"></i></button>                                        
+                    <button type="button" class="btn btn-social-icon btn-twitter btn-rounded"><i class="fa fa-twitter"></i></button>
+                    <button type="button" class="btn btn-social-icon btn-whatsapp btn-rounded"><i class="fa fa-whatsapp"></i></button>
+                  </div>
+              </div> 
+            </div>     
+    </footer>
+    <script src="app.js"></script>
+</body>
+</html>
